7cdc96da5b502032d948b12b802b7a819bc7cde3
Job Alvarez <jobitoalv@gmail.com>
2023-01-17 10:40:02 +1000

    new fucntion added

--- src/reducer.js
+++ src/reducer.js
@@ -25,14 +25,18 @@ const reducer = (state,action) => {
     }
     if(action.type === 'GET_TOTALS'){
 
-        const {total,amount} = state.cart.reduce((cartTotal,cartItem)=>{
+        let {total,amount} = state.cart.reduce((cartTotal,cartItem)=>{
             const { price, amount} = cartItem;
-            cartTotal.amount += amount
+            const itemTotal = price * amount;
+
+            cartTotal.total += itemTotal;
+            cartTotal.amount += amount;
             return cartTotal
         },{
             total:0,
             amount:0
         })
+        total = parseFloat(total.toFixed(2))
         return { ...state,total,amount }
     }
     
